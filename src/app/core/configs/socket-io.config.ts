79044a40ba5f01df9d44cfe3c166ab7a1e9e9974
Lesha <lesha@MacBook-Pro-Lesha.local>
2023-02-05 03:15:39 +0200

    fix

--- src/app/core/configs/socket-io.config.ts
+++ src/app/core/configs/socket-io.config.ts
@@ -2,7 +2,7 @@ import { environment } from "@env/environment";
 import type { SocketIoConfig } from "ngx-socket-io";
 
 const { hostname, port } = new URL(environment.apiUrl);
-const url = environment.production ? `${hostname}:${port}` : `${hostname}:${port}`;
+const url = environment.production ? `dev-api-ws.resty.od.ua` : `${hostname}:${8081 || port}`;
 
 export const SOCKET_IO_CONFIG: SocketIoConfig = {
 	url,
