65f87663fc568cc9cf70fe3a14d3c0a9a4ca803c
Viacheslav Saloid <viacheslavsaloid.work@gmail.com>
2023-02-28 00:44:54 +0200

    fix: admin

--- src/app/core/pages/admin/pages/company/pages/place/pages/active-order/resolvers/active-order-page/active-order-page.resolver.ts
+++ src/app/core/pages/admin/pages/company/pages/place/pages/active-order/resolvers/active-order-page/active-order-page.resolver.ts
@@ -1,21 +1,33 @@
 import { Injectable } from "@angular/core";
 import type { Resolve } from "@angular/router";
 import type { ActivatedRouteSnapshot } from "@angular/router";
-import { ORDER_ID } from "@shared/constants";
+import { ADMIN_ROUTES, COMPANY_ID, PLACE_ID } from "@shared/constants";
+import { RouterService } from "@shared/modules/router";
+import { catchError, of } from "rxjs";
 
 import { ActiveOrderPageGQL } from "../../graphql";
 
 @Injectable({ providedIn: "root" })
 export class ActiveOrderPageResolver implements Resolve<unknown> {
-	constructor(private readonly _activeOrderPageGQL: ActiveOrderPageGQL) {}
+	constructor(
+		private readonly _activeOrderPageGQL: ActiveOrderPageGQL,
+		private readonly _routerService: RouterService
+	) {}
 
 	resolve(activatedRouteSnapshot: ActivatedRouteSnapshot) {
-		const orderId = activatedRouteSnapshot.paramMap.get(ORDER_ID.slice(1));
+		const { orderId, companyId, placeId } = activatedRouteSnapshot.params;
 
 		if (!orderId) {
 			return;
 		}
 
-		return this._activeOrderPageGQL.fetch({ orderId });
+		return this._activeOrderPageGQL.fetch({ orderId }).pipe(
+			catchError(async () => {
+				await this._routerService.navigateByUrl(
+					ADMIN_ROUTES.ACTIVE_ORDERS.absolutePath.replace(COMPANY_ID, companyId).replace(PLACE_ID, placeId)
+				);
+				return of(null);
+			})
+		);
 	}
 }
